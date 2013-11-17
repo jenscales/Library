92d2a3887eb711b5d76107ffecd4b0d4768640d5
MakerSquare Student <students@themakersquare.com>
2013-11-17 17:04:43 -0600

    check_out method working now if user has more than 2 books. Updated some comments in all Classes

--- library.rb
+++ library.rb
@@ -2,6 +2,7 @@
 class Library
   def initialize
     @books = []
+    puts "Our new Library is now Open! Let's read."
   end
 
   #This will list out all books in the Library by Title, Author and Status
@@ -14,8 +15,8 @@ class Library
   #This checks a books status and if it is checked out will return the borrower's name and book title.
   def borrowed_books
     @books.each do |book|
-      if book.status == 'checked out'
-        puts "Sorry, This book #{book.title} is not available today!"
+      if book.status == "checked out"
+        puts "Sorry, The book '#{book.title}' is not available today!"
         puts "The following borrower: #{book.borrower.name} has this book checked out"
       end
     end
@@ -24,8 +25,8 @@ class Library
   # This will print what books are currently available for checkout
   def available_books
     @books.each do |book|
-      if book.status == 'available'
-        puts "Good News! This book #{book.title} is available today!"
+      if book.status == "available"
+        puts "Good News! This book '#{book.title}' is available today!"
       end  
     end
   end
@@ -39,7 +40,8 @@ class Library
   #This will check if the user can borrow a book.
   def check_out(user, book)
     if user.borrowed_books.length == 2
-      return "Sorry, that user #{user} already has two books checked out."
+      puts "Sorry, the user #{user.name} already has two books checked out."
+      return
     end
 
     if book.status == "available"
@@ -48,9 +50,10 @@ class Library
       user.borrow_book(book)
       puts "Great #{book.borrower.name}, you just checked out a book: #{book.title}"
     else
-      puts "Sorry #{book.borrower.name}, this book #{book.title} is checked out!"
+      puts "Sorry #{book.borrower.name}, the book '#{book.title}' is checked out!"
     end
   end
+
 
   #This will check in a book back to the Library
   def check_in(book)
@@ -59,12 +62,12 @@ class Library
     end
 
     if book.status == "checked out"
-      puts "Thank you for returning the #{book} today! Come again."
-      user.borrow_book.delete(book)
+      puts "Thank you #{user} for returning the '#{book}' today! Come again."
+      user.borrowed_books.delete(book)
       book.status = "available"
     end
   end
-
+#This is the end of the Library Class
 end
 
 #This creates a new borrower. Assigns books as empty. Assigns name. Welcomes borrower to library.
@@ -74,36 +77,43 @@ class Borrower
     @books = []
     puts "Welcome new borrower #{name} to the Library"
   end
+
   #This shows what books the borrower currently has.
   def borrowed_books
     @books
   end
+
   #This will add a book to the borrower's account.
   def borrow_book(book)
     @books << book
   end 
+
   #This creates a variable name for the borrower's name.  
   def name
     @name
   end
+
   #This sets and prints the books currently borrowed by the user.
   def borrowed_books_count
     books.length
   end
+
   #This prints out the title and author of each book the borrower has.
   def borrowed_books_list
     @books.each do |book|
         puts  "This book is checked out: #{book.title} by #{book.author}"
     end
   end
+#This is the end of the Borrower Class
 end
 
+#This defines a book with in the library
 class Book
   #attr_reader :title, :author
   def initialize(title, author)
     @title = title
     @author = author
-    puts "You have just added a new book: #{title} : #{author}."
+    puts "You have just added a new book: #{title} by #{author}."
     @status = 'available'
     @borrower = nil
   end
